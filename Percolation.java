e59a00889099e6aaa6d7c096511a0b3ef28aabd6
phuongha2012 <hami201289@him-mail.ch>
2020-07-17 15:05:10 +1200

    API definition for Percolation problem

---
+++ Percolation.java
@@ -0,0 +1,33 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    public Percolation(int N) {
+
+    }
+
+    private int getSingleArrayIndex(int i, int j) {
+
+    }
+
+    public void open(int i, int j) {
+
+    }
+
+    public boolean isOpen(int i, int j) {
+
+    }
+
+    public boolean isFull(int i, int j) {
+
+    }
+
+    public int numberOfOpenSites() {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+}
